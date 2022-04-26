f1ba9b775db2a53aad74889469be7aa56a1100ae
luoluo <750391998@qq.com>
2022-04-26 13:18:16 +0800

    完成了主页功能的开发

--- js/baseAPI.js
+++ js/baseAPI.js
@@ -6,4 +6,25 @@ $.ajaxPrefilter(function(options){
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
     console.log(options.url);
+
+    // 统一为有权限的接口设置 headers 请求头
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    // 不管成功或者是失败，都会执行 complete
+    options.complete = function(res) {
+        console.log(res);
+            // 在 complete 回调函数中，可以用 responseJSON 拿到服务器的响应数据
+            if(res.responseJSON.status == 1 || res.responseJSON.message == '身份认证失败！') {
+                // 强制清空token
+                localStorage.removeItem('token');
+                // 强制跳转到登录页面
+                location.href = 'login.html';
+            }
+    }
+    
 })
---
+++ js/index.js
@@ -0,0 +1,62 @@
+$(function(){
+    // 调用 getUserInfo() 获取用户的基本信息
+    getUserInfo();
+
+    // 点击退出按钮，退出登录
+    $('#btnLogout').on('click',function() {
+        // 提示用户是否退出
+        layer.confirm('确定退出登录？', {icon: 3, title:'提示'}, function(index){
+            //do something
+            console.log(1111);
+            // 清空本地存储中的token
+            localStorage.removeItem('token');
+            // 重新跳转到登录页面
+            location.href = 'login.html';
+
+            layer.close(index);
+        });     
+          
+    })
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        // 这个 url 地址的前半段在 baseAPI.js 中拼接了
+        url: '/my/userinfo',
+        method: 'GET',
+        // headers 就是请求头配置对象
+        // 这里的 headers 请求头放到了 baseAPI.js 中
+        // 像 login.js 中的请求 URL地址拼接一样麻烦，就封装到了 baseAPI.js 中
+        success: function(res){
+            console.log(res);
+            if(res.status != 0){
+                // return console.log(res.message);
+                return layer.msg('获取用户基本信息失败');
+            }
+            console.log('获取用户基本信息成功！');
+            // 调用 renderAvatar 渲染用户头像
+            renderAvatar(res.data);
+        },
+        
+        // complete: function(res) {
+            
+        // }
+    })
+}
+
+function renderAvatar(user) {
+    // 获取用户的名称
+    var name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp' + name);
+    // 渲染图片头像
+    if(user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    }else {
+        // 渲染文本头像
+        $('.layui-nav-img').hide();
+        var frist = name[0].toUpperCase();
+        $('.text-avatar').html(frist).show();
+    }
+    
+}
